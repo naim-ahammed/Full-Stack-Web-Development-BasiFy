4ba65f7d2c5465782429460b155210925338a710
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2024-11-17 08:33:58 +0600

    creating a personal portfolio website

---
+++ Web-Programming-Lab/lab-report-5/sctipt.js
@@ -0,0 +1,9 @@
+function showEmail() {
+    var mail = document.getElementById(".email");
+    if(mail.style.display === 'none'){
+     mail.style.display = 'block';
+    }else{
+     mail.style.display = 'none';
+    }
+  }
+  
