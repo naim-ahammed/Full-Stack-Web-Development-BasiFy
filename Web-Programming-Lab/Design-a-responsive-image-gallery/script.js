8ba3c4f037eb053eeeb13cc22ad0d40ece0390c7
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2024-11-30 22:11:42 +0600

    Design a responsive image gallery using CSS Grid and JavaScript.

--- Web-Programming-Lab/Design-a-responsive-image-gallery/script.js
+++ Web-Programming-Lab/Design-a-responsive-image-gallery/script.js
@@ -26,3 +26,54 @@ modal.addEventListener("click", (e) => {
         modal.style.display = "none";
     }
 });
+
+
+
+// slider start
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active =0;
+let lengthItems = items.length - 1;
+
+next.onclick = function(){
+    // active +=1;
+    if(active+1>lengthItems){
+        active = 0;
+    }else{
+        active = active +1;
+    }
+    reloadSlider();
+}
+prev.onclick = function (){
+    if(active-1<0){
+        active = lengthItems;
+    }else{
+        active = active -1;
+    }
+}
+
+// let reloadSlider = setInterval(()=>{
+//     next.click()
+// },3000 );
+
+function reloadSlider(){
+    let cheakleft = items[active].offsetLeft;
+    list.style.left = -cheakleft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots.classList.remove('active');
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function(){
+        active=key;
+        reloadSlider();
+    })
+})
+
+// slider end
