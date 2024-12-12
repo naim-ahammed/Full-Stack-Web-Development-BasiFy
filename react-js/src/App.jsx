7c2f066f748173b90d9bc0462b05652f5cb099f4
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2024-12-12 22:51:34 +0600

    cheak condition

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -14,16 +14,32 @@
 // import React from 'react'
 // import Navbar from "./components/Navbar"
 
+// 
+// const App = () => {
+
+//   let x = "NaimAhammed";
+//   let array = ["apple", "mango", "banana"]
+
+//   return (
+//     <div>
+//       {x}
+//       {array.map((user) => {return <h2>{user}</h2>})}
+//     </div> 
+//   )
+// }
+
+// export default App
+// 
+
 const App = () => {
 
-  let x = "Naim Ahammed";
-  let array = ["apple", "mango", "banana"]
+  let data = "boy";
 
   return (
     <div>
-      {x}
-      {array.map((user) => {return <h2>{user}</h2>})}
+      {data==="boy"?<h1>Boy</h1>:<h1>Girl</h1>}
     </div>
+    
   )
 }
 
