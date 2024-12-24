868c70c7c144ac95125b31ce19d46d94fbca6fb2
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2024-12-24 12:33:11 +0600

    React Components

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,4 +1,5 @@
-// 
+//rfc ---> React Function
+//rfce ---> reactFunctiona1ExportComponent
 
 
 // const App = () => {
@@ -17,7 +18,7 @@
 // 
 // const App = () => {
 
-//   let x = "NaimAhammed";
+//   let x = "Naim Ahammed";
 //   let array = ["apple", "mango", "banana"]
 
 //   return (
@@ -31,16 +32,153 @@
 // export default App
 // 
 
+
+// const App = () => {
+
+//   let data = "boy";
+
+//   return (
+//     <div>
+//       {data==="boy"?<h1>Boy</h1>:<h1>Girl</h1>}
+//     </div>
+    
+//   )
+// }
+
+// export default App
+
+
+// const App = () => {
+
+//   const onClickBtn = ()=>{
+//     console.log("click ");
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={()=>{onClickBtn()}}>Click me</button>
+//     </div>
+    
+//   )
+// }
+
+// export default App
+
+
+
+// const App = () => {
+
+
+//   let x = 0;
+
+//   const onClickBtn = ()=>{
+//     console.log("click ");
+//     x++;
+//     console.log(x);
+
+//   }
+
+//   return (
+//     <div>
+//       {x}
+//       <button onClick={()=>{onClickBtn()}}>Click me</button>
+//     </div>
+    
+//   )
+// }
+
+// export default App
+
+
+// import {useState} from "react";
+// import FristComponent from "./components/FristComponent";
+
+// const App = () => {
+
+//   // const [x,setx] = useState(0);
+
+//   const [x,setx] = useState(0);
+
+
+//   const onClickBtn = ()=>{
+//     console.log("click ");
+//     setx(x+1);
+//     console.log(x);
+
+//   }
+
+//   return (
+//     <div>
+//       {/* {x} */}
+//       <button onClick={()=>{onClickBtn()}}>Click me</button>
+//       <FristComponent data={x}/>
+//     </div>
+    
+//   )
+// }
+
+// export default App
+
+
+
+// import {useState} from "react";
+// import FristComponent from "./components/FristComponent";
+
+// const App = () => {
+
+//   // const [x,setx] = useState(0);
+
+//   const [x,setx] = useState(0);
+
+
+//   const onClickBtn = ()=>{
+//     console.log("click ");
+//     setx(x+1);
+//     console.log(x);
+
+//   }
+
+//   var tempStyle ={
+//     color:'red',
+//     backgroundColor:'blue'
+
+//   }
+
+//   return (
+//     <div className="div" style={tempStyle}>
+//       {/* {x} */}
+//       <button style={{color:'red'}} onClick={()=>{onClickBtn()}}>Click me</button>
+//       <FristComponent data={x} fn={setx}/>
+//     </div>
+    
+//   )
+// }
+
+// export default App
+
+
+
+import React from 'react';
+
 const App = () => {
-
-  let data = "boy";
-
   return (
     <div>
-      {data==="boy"?<h1>Boy</h1>:<h1>Girl</h1>}
+      <FunctionName></FunctionName>
     </div>
-    
-  )
+  );
 }
 
-export default App
+//React Components
+function FunctionName(){
+  return(
+    <div>
+    <h1>function Name</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
